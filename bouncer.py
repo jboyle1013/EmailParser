9bdb2ab84c7aab4f044be673a05a5d9bc7075bef
jboyle1013 <58829335+jboyle1013@users.noreply.github.com>
2023-02-28 10:21:08 -0500

    bouncer take 3

--- bouncer.py
+++ bouncer.py
@@ -4,7 +4,7 @@ from emailparser import *
 # The purpose of these functions is to control how many emails enter the parser at any given time
 # This code will be used to run the
 
-def makebatches(tf):
+def makebatchest(tf):
     directory = "//email-analysis-data/new-emails"
     folder_names = [f.name for f in os.scandir(directory) if f.is_dir()]
     # sort the folder names in alphabetical order
@@ -26,3 +26,50 @@ def makebatches(tf):
             shutil.move(norigpath, ndistpath)
 
 
+def makebatches(tf):
+    directory = "//email-analysis-data/new-emails"
+    folder_names = [f.name for f in os.scandir(directory) if f.is_dir()]
+    # sort the folder names in alphabetical order
+    folder_names.sort()
+    sublists = {}
+
+    for name in folder_names:
+        first_letter = name[0].lower()
+        if first_letter < 'a' or first_letter > 'z':
+            continue
+        if first_letter <= 'g':
+            sublist_key = 'a-g'
+        elif first_letter <= 'n':
+            sublist_key = 'h-n'
+        elif first_letter <= 'u':
+            sublist_key = 'o-u'
+        else:
+            sublist_key = 'v-z'
+        if sublist_key not in sublists:
+            sublists[sublist_key] = []
+        sublists[sublist_key].append(value)
+
+    for list in sublists.keys():
+        for names in sublists.get(list):
+            dir_name = directory + "/" + names
+            fsetup = "/root/email-analysis-data/new-emails" + "/" + names + "/"
+            if not os.path.exists( fsetup ):  # Does the Directory already path exist?
+                os.mkdir( fsetup)  # Make directory path
+            foptions = ["new", "curr", "temp"]
+            for opt in foptions:
+                fpath = fsetup + opt + "/"
+                if not os.path.exists( fpath ): # Does the Directory already path exist?
+                    os.mkdir( fpath)  # Make directory path
+            distpath = "/root/email-analysis-data/new-emails" + "/" + names + "/new/"
+            for file in sorted(os.scandir(directory), key=lambda e: e.name):
+                file_str = str(file.name)  # Turns the name of the entry to a string
+                origpath = dir_name + "/" + file_str
+                shutil.move(origpath, distpath)
+        emailparser(tf)
+        for file in sorted(os.scandir(distpath), key=lambda e: e.name):
+            ndistpath = f"//email-analysis-data/ processed-emails/{names}/"
+            if not os.path.exists( ndistpath ):  # Does the Directory already path exist?
+                os.mkdir( ndistpath)  # Make directory path
+            file_str = str(file.name)  # Turns the name of the entry to a string
+            norigpath = distpath + file_str
+            shutil.move(norigpath, ndistpath)
