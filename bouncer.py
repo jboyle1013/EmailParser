789103a593d920eceab7e699eedacfa06088fb1c
Jordan Boyle <jboyle1013@vt.edu>
2023-02-28 13:40:31 -0500

    fixed path error

--- bouncer.py
+++ bouncer.py
@@ -1,29 +1,6 @@
 import shutil
 from emailparser import *
 # The purpose of these functions is to control how many emails enter the parser at any given time
-# This code will be used to run the
-
-def makebatchest(tf):
-    directory = "//email-analysis-data/new-emails"
-    folder_names = [f.name for f in os.scandir(directory) if f.is_dir()]
-    # sort the folder names in alphabetical order
-    folder_names.sort()
-    for name in folder_names:
-        dir_name = directory + "/" + name
-        distpath = "/root/email-analysis-data/new-emails" + "/" + name + "/new/"
-        for file in sorted(os.scandir(directory), key=lambda e: e.name):
-            file_str = str(file.name)  # Turns the name of the entry to a string
-            origpath = dir_name + "/" + file_str
-            shutil.move(origpath, distpath)
-        emailparser(tf)
-        for file in sorted(os.scandir(distpath), key=lambda e: e.name):
-            ndistpath = f"//email-analysis-data/ processed-emails/{name}/"
-            if not os.path.exists( ndistpath ):  # Does the Directory already path exist?
-                os.mkdir( ndistpath)  # Make directory path
-            file_str = str(file.name)  # Turns the name of the entry to a string
-            norigpath = distpath + file_str
-            shutil.move(norigpath, ndistpath)
-
 
 def makebatches(tf):
     directory = "//email-analysis-data/new-emails"
--- startbycron.py
+++ startbycron.py
@@ -2,7 +2,7 @@ from emailparser import emailparser
 from bouncer import makebatches
 import sys, getopt
 
-# This is the function that for the cron
+# This is the function that the cron job will use to start the parser. It has no arguments.
 def startbycron(tf):
 
     makebatches(tf)
