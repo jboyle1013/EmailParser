7c4ccd08d0315e5d5440b0b4fe472288179bce24
jboyle1013 <58829335+jboyle1013@users.noreply.github.com>
2023-03-01 11:43:32 -0500

    fixing filereplacer

--- replacefiles.py
+++ replacefiles.py
@@ -9,11 +9,13 @@ def filereplacer():
     dest_name = "//email-analysis-data/new-emails"
     for folder in sorted(os.scandir(dir_name), key=lambda e: e.name):
         f_str = str(folder.name)  # Turns the name of the folder to a string
-        dirpath = dir_name + "/" + f_str
+        dirpath = dest_name + "/" + f_str
+        if not os.path.exists( dirpath ):  # Does the Directory already path exist?
+            os.mkdir( dirpath)  # Make directory path
         for file in sorted(os.scandir(dirpath), key=lambda e: e.name):
             file_str = str(file.name)  # Turns the name of the file to a string
-            fd_path = dest_name + "/" + f_str + "/"
-            fopath = dirpath + "/" + file_str
+            fd_path = dirpath + "/"
+            fopath = dir_name + "/" + f_str + "/" + file_str
             try:
                 shutil.move(fopath, fd_path)
             except:
